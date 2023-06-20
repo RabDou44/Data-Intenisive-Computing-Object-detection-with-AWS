e6ce0af9dcdac599cc4fec8c5805cdc1ecee830e
dgiovannini <dario.giovannini@gmx.at>
2023-06-20 18:29:56 +0200

    initial testing + impl. of object detection

--- app.py
+++ app.py
@@ -28,27 +28,52 @@ from io import BytesIO
 from flask import Flask, request, Response, jsonify
 import random
 import re
+import tensorflow as tf
+import tensorflow_hub as hub
 
 
 app = Flask(__name__)
 
-
+default_module = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
+detector = hub.load(default_module).signatures['default']
 
 def detection_loop(filename_image):
-   #TODO
-   pass
-   """
-    data = {
-        "status": 200,
-        "bounding_boxes": bounding_boxes,
-        "inf_time": inf_times,
-        "avg_inf_time": str(avg_inf_time),
-        "upload_time": upload_times,
-        "avg_upload_time": str(avg_upload_time),
-        
-    }
-    return make_response(jsonify(data), 200)
-    """
+
+  def load_img(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    return img
+
+  img = load_img(filename_image)
+  converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+
+  start_time = time.time()
+  result = detector(converted_img)
+  inf_time = start_time - time.time()
+
+  result = {key:value.numpy() for key,value in result.items()}
+
+  bounding_boxes = [{"score":s, "label":l, "bounding_box":bb} 
+                    for s,l,bb in zip(result["detection_scores"],
+                                      result["detection_class_entities"],
+                                      result["detection_boxes"])]
+  
+  return {"inference_time":inf_time, 
+          "bounding_boxes":bounding_boxes}
+
+  #TODO - make actual json response?
+  """ this is from the 
+  data = {
+      "status": 200,
+      "bounding_boxes": bounding_boxes,
+      "inf_time": inf_times,
+      "avg_inf_time": str(avg_inf_time),
+      "upload_time": upload_times,
+      "avg_upload_time": str(avg_upload_time),
+      
+  }
+  return make_response(jsonify(data), 200)
+  """
 
 #initializing the flask app
 app = Flask(__name__)
@@ -67,7 +92,7 @@ def main():
     images.append((np.array(Image.open(io.BytesIO(base64.b64decode(img))),dtype=np.float32)))
   
   
-  return detection_loop(filename_image)
+  return detection_loop(images)
   
 # status_code = Response(status = 200)
 #  return status_code
